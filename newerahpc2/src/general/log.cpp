e5384dd73fb5982df0ca6acbc6c913c6988a93fe
varun <varun@04e49310-7c1c-b66c-f803-70fc42c164db>
2010-10-07 01:18:08 +0000

    git-svn-id: https://newerahpc.googlecode.com/svn/trunk@4 04e49310-7c1c-b66c-f803-70fc42c164db

---
+++ newerahpc2/src/general/log.cpp
@@ -0,0 +1,31 @@
+//Copyright 2010 Varun Mittal ©
+//newerahpc program is distributed under the terms of the GNU General Public License
+//
+//This file is part of newerahpc.
+//
+//newerahpc is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation version 2 of the License.
+//
+//newerahpc is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with newerahpc.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <network.h>
+#include <time.h>
+
+namespace newera_network{
+#ifdef debug
+	std::ofstream log_file("log_file");
+#endif
+	void add_log(const char *str){
+		time_t rawtime;
+		time(&rawtime);
+		log_file<<ctime(&rawtime)<<" "<<str<<endl;
+	}
+};
+

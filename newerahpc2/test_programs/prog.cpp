9a405155f138745ba4de1181bcaeb60c1a05cdda
varunmittal91 <varunmittal91@04e49310-7c1c-b66c-f803-70fc42c164db>
2010-10-07 10:40:23 +0000

    git-svn-id: https://newerahpc.googlecode.com/svn/trunk@8 04e49310-7c1c-b66c-f803-70fc42c164db

--- newerahpc2/test_programs/prog.cpp
+++ newerahpc2/test_programs/prog.cpp
@@ -10,11 +10,17 @@ int main(){
 	grid_task task1;
 	task1.func_name = (char *)"newera_task";
 	instruction_set *in1 = new instruction_set;
+	instruction_set *in2 = new instruction_set;
 	int *data = new int;
+	int *data_2 = new int;
 	*data = 12;
+	*data_2 = 13;
 	in1->length = sizeof(int);
 	in1->data = (void *)data;
+	in2->length = sizeof(int);
+	in2->data = (void *)data_2;
 	task1 = in1;
+	task1 = in2;
 	task1.submit();
 	task1.wait();
 	return 0;

a57f018959709b2d638638c5bf9a0e7f9623eb6a
varunmittal91 <varunmittal91@04e49310-7c1c-b66c-f803-70fc42c164db>
2011-12-13 03:14:30 +0000

    git-svn-id: https://newerahpc.googlecode.com/svn/trunk@169 04e49310-7c1c-b66c-f803-70fc42c164db

---
+++ neweraHPC3/src/grid/grid_nxi.cpp
@@ -0,0 +1,39 @@
+/*
+ *	(C) 2011 Varun Mittal <varunmittal91@gmail.com> & Varun Dhawan <varundhawan5792@gmail.com>
+ *	NeweraHPC program is distributed under the terms of the GNU General Public License v2
+ *
+ *	This file is part of NeweraHPC.
+ *
+ *	NeweraHPC is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation version 2 of the License.
+ *
+ *	NeweraHPC is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with NeweraHPC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include <include/grid.h>
+
+using namespace std;
+
+namespace neweraHPC
+{
+   nhpc_status_t nhpc_check_nxi(const char *file_path)
+   {
+      nhpc_status_t nrv = nhpc_strcmp(file_path, "*.nxi");
+
+      return nrv;
+   }
+   
+   char *nhpc_nxitodll(const char *file_path)
+   {
+      cout<<file_path<<endl;
+   }
+};

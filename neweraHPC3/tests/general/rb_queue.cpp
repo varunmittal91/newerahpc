c5ec42d45a810c24c6f8f7e22a14c70d033d01b5
varundhawan5792 <varundhawan5792@04e49310-7c1c-b66c-f803-70fc42c164db>
2011-12-14 09:48:09 +0000

    git-svn-id: https://newerahpc.googlecode.com/svn/trunk@175 04e49310-7c1c-b66c-f803-70fc42c164db

---
+++ neweraHPC3/tests/general/rb_queue.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <include/rbtree.h>
+
+using namespace std;
+using namespace neweraHPC;
+
+int main(){
+
+   rbtree_t *tree = new rbtree_t[10];
+   return 0;
+}

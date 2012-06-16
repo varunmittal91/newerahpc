578a453c565cc57417522c85c8f2dfd50b623a62
varunmittal91 <varunmittal91@04e49310-7c1c-b66c-f803-70fc42c164db>
2012-06-16 15:53:49 +0000

    git-svn-id: https://newerahpc.googlecode.com/svn/trunk@304 04e49310-7c1c-b66c-f803-70fc42c164db

---
+++ neweraHPC3/src/general/alloc.cpp
@@ -0,0 +1,68 @@
+/*
+ *	(C) 2011 Varun Mittal <varunmittal91@gmail.com>
+ *	NeweraHPC program is distributed under the terms of the GNU General Public License v2
+ *
+ *	This file is part of NeweraHPC.
+ *
+ *	NeweraHPC is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation version 2 of the License.
+ *
+ *	NeweraHPC is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with NeweraHPC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <errno.h>
+
+#include <include/alloc.h>
+
+using namespace std;
+
+namespace neweraHPC
+{
+   void *operator new(std::size_t size)
+   {
+      LOG_INFO("using overloaded new");
+      
+      void *new_p = NULL;
+      
+      if(garbage_collector_ready)
+	 new_p = nhpc_allocate_str(size);
+      else 
+      {
+	 new_p = malloc(size);
+	 memset(new_p, 0, size);
+      }
+      
+      if(!new_p)
+      {
+	 LOG_ERROR("Allocation error, errno: " << errno);
+	 exit(0);
+      }
+      
+      return new_p;
+   }
+   
+   void *operator new[](std::size_t size)
+   {
+      void *new_p = operator new(size);
+      return new_p;
+   }
+   
+   void operator delete(void *ptr)
+   {
+      if(garbage_collector_ready)
+	 nhpc_deallocate_str((char *)ptr);
+   }
+   
+   void operator delete[](void *ptr)
+   {
+      operator delete(ptr);
+   }   
+}
